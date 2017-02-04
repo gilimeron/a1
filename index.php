2dabc095c712dfe72f04049fbb8941ac833610c2
Gili Meron <gili.meron@gmail.com>
2017-02-03 21:09:38 -0500

    basic index and css

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Assignment 1</title>
+ </head>
+ <body>
+  <h1>Gili Meron</h1>
+  <img src='/images/Abigail.jpg' alt='Adorable baby'>
+  <h2>About Me</h2>
+  <p>I am a Quality Assurance Engineer in a cloud security startup. I live in Newton MA and studying towards a degree in Information System Management. I have a husband, an adorable 5 months old baby, and a cat. </p>
+  <h2>Random Quote>
+  <p></p>
+ </body>
+</html>
