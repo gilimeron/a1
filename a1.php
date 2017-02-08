df3df5c653de8a526ae69fea36b573b6594cc362
Gili Meron <gili.meron@gmail.com>
2017-02-07 20:44:55 -0500

    updated img file name

--- a1.php
+++ a1.php
@@ -12,7 +12,7 @@
 
  <body>
   <h1>Gili Meron</h1>
-  <img src='/images/Gili.jpg' alt='Me'>
+  <img src='/images/gili.jpg' alt='Me'>
   <h2>About Me</h2>
   <p>I am a Quality Assurance Engineer at CloudLock, a cloud security company recently acquired by Cisco. <br/> I'm taking this course as part of my master's degree in Information Systems Management in Harvard Extension School. By taking this course I hope to expand my knowledge in building web applications.
   <br/>I live in Newton MA with my husband, our adorable 5 months old baby girl, and a cat named Sushi. </p>
