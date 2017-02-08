f154b904836b7a601d8dcea0d3d38276262f39c4
Gili Meron <gili.meron@gmail.com>
2017-02-07 20:33:59 -0500

    replaced photo and finalized all text and css

--- a1.php
+++ a1.php
@@ -3,17 +3,19 @@
 <!DOCTYPE html>
 
 <html>
+
  <head>
-  <title>Assignment 1</title>
+  <title>Gili Meron</title>
   <link rel="stylesheet" href="style.css">
   <meta charset='utf-8'>
  </head>
 
  <body>
   <h1>Gili Meron</h1>
-  <img src='/images/Abigail.jpg' alt='Adorable baby'>
+  <img src='/images/Gili.jpg' alt='Me'>
   <h2>About Me</h2>
-  <p>I am a Quality Assurance Engineer in a cloud security startup. I live in Newton MA and studying towards a degree in Information System Management. I have a husband, an adorable 5 months old baby, and a cat. </p>
+  <p>I am a Quality Assurance Engineer at CloudLock, a cloud security company recently acquired by Cisco. <br/> I'm taking this course as part of my master's degree in Information Systems Management in Harvard Extension School. By taking this course I hope to expand my knowledge in building web applications.
+  <br/>I live in Newton MA with my husband, our adorable 5 months old baby girl, and a cat named Sushi. </p>
   <h2>Random Quote</h2>
   <p>
     <?php
--- quoteGenerator.php
+++ quoteGenerator.php
@@ -1,8 +1,7 @@
 <?php
 
-  $quotes = ['Life is like a box of chocolates - you never know what you are going to get', 'You know nothing John Snow!', 'A girl has no name'];
+  $quotes = ['Normal is an illusion. What is normal for the spider is chaos for the fly. - Morticia Addams', 'We build too many walls and not enough bridges. - Isaac Newton', 'Success is walking from failure to failure with no loss of enthusiasm. - Winston Churchill'];
   function generateQuote ($quotes) {
       $quoteNumber = array_rand ($quotes,1);
       echo $quotes[$quoteNumber];
     }
-?>
